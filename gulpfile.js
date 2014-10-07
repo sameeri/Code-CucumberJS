27fe094f511584596247a8c69c83c86a53dd07b1
Sameeri Marrboyina <smarryboyina@telogical.com>
2014-10-07 16:16:24 -0500

    Making the gulp changes to actually run the cucumberjs

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,24 @@
 'use strict';
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    child_process = require('child_process');
+
+var options = [ 'node_modules/cucumber/bin/cucumber.js',
+    'features',
+    '-r', 'features/step_definitions',
+    '-f', 'pretty'];
 
 function runSpecifications() {
-    console.log('Running cucumber specifications');
+    console.log('Now Running cucumber specifications');
+    var cucumber = child_process.spawn('node', options)
+        .on('exit', function() {
+        });
+    cucumber.stdout.on('data', function(d) {
+        console.log(String(d));
+    });
+
+    cucumber.stderr.on('data', function(d) {
+        console.error(String(d));
+    });
 }
 
 gulp.task('bdd-spec', runSpecifications);
